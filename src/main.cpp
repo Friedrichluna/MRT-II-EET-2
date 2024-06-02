c34623dbe8c998aca2872cf4e520b9dfcb793b1d
Friedrich Lunacek <johanschulz77@web.de>
2024-06-02 17:32:09 +0200

    Aufgabe 5.2

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,24 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+
+
+int ein = 0;
+int aus = 1;
+
 
 void setup ( ) {
-  //Initialisiert Pin 14 (LED1) als output .
+  //Initialisiert Pin 14 (LED1) als output und Pin 0 und 1 als Input
   pinMode(14, OUTPUT);
+  pinMode(0, INPUT);
+  pinMode(1, INPUT);
 }
 
 // LED1 blinkt im Zweisekundentakt
 void loop(){
-  digitalWrite(14, HIGH); // LED1 an
-  delay(1000); // 1000 ms warten
-  digitalWrite(14, LOW); // LED1 aus
-  delay(1000); // 1000 ms warten
+  
+  
+  if(digitalRead(ein)) digitalWrite(14, HIGH); // LED1 an, wenn Taster 1 gedrückt wird
+  
+  if(digitalRead(aus)) digitalWrite(14, LOW); // LED1 aus, wenn Taster 2 gedrückt wird
+
 }
